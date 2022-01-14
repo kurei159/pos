897d77cac1ec27b7855a9f4670871c4f98fa0dd4
Vincent Chau <cheukwang.chau@gmail.com>
2022-01-13 23:32:32 -0600

    WIP load JSON menu temporarily

--- src/components/order_menu/orderMenu.jsx
+++ src/components/order_menu/orderMenu.jsx
@@ -1,6 +1,8 @@
 import React, { Component }from 'react'
 import { Card, CardActions, Stack, Button, ButtonGroup } from "@mui/material"
 import { styled } from '@mui/styles';
+import Toolbar from '@mui/material/Toolbar';
+import Divider from '@mui/material/Divider';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ClearIcon from '@mui/icons-material/Clear';
 import DeliveryDiningOutlinedIcon from '@mui/icons-material/DeliveryDiningOutlined';
@@ -15,58 +17,63 @@ const CategoryBtn = styled(Button) ({
     float: "right"
 })
 
-const Categories = new Map();
+function clickedCat(props) {
+    console.log("Clicked " + props);
 
-function getMenuCategories(menu) {
-    for(var i = 0; i < Object.keys(menu.categories).length; i++) {
-
-    }
 }
 
 class orderMenu extends Component {
-    componentDidMount() {
-        var menu = menuFile;
-        getMenuCategories(menu);
-    }
     render() {
         return (<div>
             <Card className="orderList_menu" sx={{ boxShadow: 3 }}>
-                <CardActions>
-                    <Stack direction="row" spacing={1}>
-                    <Button variant="outlined" >
-                        <Link to="/" style={{ textDecoration: 'none', color: 'WhiteSmoke' }}>
-                            <ArrowBackIosNewIcon/>BACK
-                        </Link>
-                        </Button>
-                        <Button variant="outlined" color="error">
-                            <Link to="/" style={{ textDecoration: 'none', color: 'red'}}>
-                                <ClearIcon/>CLEAR
+                <Stack direction="column">
+                <Toolbar disableGutters>
+                    <CardActions>
+                        <Stack direction="row" spacing={1}>
+                        <Button variant="outlined" >
+                            <Link to="/" style={{ textDecoration: 'none', color: 'WhiteSmoke' }}>
+                                <ArrowBackIosNewIcon/><br/>BACK
                             </Link>
-                        </Button>
-                        <Button variant="outlined" color="warning">
-                            <Link to="/" style={{ textDecoration: 'none', color:'DarkOrange'}}>
-                                <DeliveryDiningOutlinedIcon/>DELIVERY
-                            </Link>
-                        </Button>
-                        <Button variant="outlined">
-                            <Link to="/" style={{ textDecoration: 'none', color: 'SkyBlue'}}>
-                                <ListAltOutlinedIcon/>RECALL
-                            </Link>
-                        </Button>
-                        <Button variant="outlined">
-                            <Link to="/" style={{ textDecoration: 'none', color: 'SkyBlue'}}>
-                                <EditOutlinedIcon/><br/>OPEN ITEM
-                            </Link>
-                        </Button>
-                        <Button variant="outlined" color="success">
-                        <Link to="/" style={{ textDecoration: 'none', color: 'SpringGreen'}}>
-                        <SendOutlinedIcon/>SEND
-                        </Link></Button>
-                    </Stack>
-                </CardActions>
-                <CardActions>
-                
-                </CardActions>
+                            </Button>
+                            <Button variant="outlined" color="error">
+                                <Link to="/" style={{ textDecoration: 'none', color: 'red'}}>
+                                    <ClearIcon/><br/>CLEAR
+                                </Link>
+                            </Button>
+                            <Button variant="outlined" color="warning">
+                                <Link to="/" style={{ textDecoration: 'none', color:'DarkOrange'}}>
+                                    <DeliveryDiningOutlinedIcon/><br/>DELIVERY
+                                </Link>
+                            </Button>
+                            <Button variant="outlined">
+                                <Link to="/" style={{ textDecoration: 'none', color: 'SkyBlue'}}>
+                                    <ListAltOutlinedIcon/><br/>RECALL
+                                </Link>
+                            </Button>
+                            <Button variant="outlined">
+                                <Link to="/" style={{ textDecoration: 'none', color: 'SkyBlue'}}>
+                                    <EditOutlinedIcon/><br/>OPEN ITEM
+                                </Link>
+                            </Button>
+                            <Button variant="outlined" color="success">
+                            <Link to="/" style={{ textDecoration: 'none', color: 'SpringGreen'}}>
+                            <SendOutlinedIcon/><br/>SEND
+                            </Link></Button>
+                        </Stack>
+                    </CardActions>
+                </Toolbar>
+                <Divider style={{marginTop: "2%", marginBottom: "2%"}} />
+                <Toolbar>
+                    <CardActions>
+                    {
+                        menuFile.map((props) => {
+                            return (
+                                <Button key={props.cat_id} onClick={() => clickedCat(props.cat_id)}>{props.name}</Button>
+                            )})
+                    }
+                    </CardActions>
+                </Toolbar>
+                </Stack>
             </Card>
         </div>)
     }
