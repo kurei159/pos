a222d1d33e28464e50fad11c64ce2b30da5dca61
Vincent Chau <cheukwang.chau@gmail.com>
2022-01-13 21:46:54 -0600

    WIP load Json Menu temporarily

--- src/components/order_menu/orderMenu.jsx
+++ src/components/order_menu/orderMenu.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Card, CardActions, Stack, Button } from "@mui/material"
+import React, { Component }from 'react'
+import { Card, CardActions, Stack, Button, ButtonGroup } from "@mui/material"
 import { styled } from '@mui/styles';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ClearIcon from '@mui/icons-material/Clear';
@@ -9,14 +9,27 @@ import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
 import { Link } from 'react-router-dom';
 import "./orderMenu.css"
+import menuFile from "../../menu_json/menu.json"
 
 const CategoryBtn = styled(Button) ({
     float: "right"
 })
 
-const orderMenu = () => {
-    return (
-        <div>
+const Categories = new Map();
+
+function getMenuCategories(menu) {
+    for(var i = 0; i < Object.keys(menu.categories).length; i++) {
+
+    }
+}
+
+class orderMenu extends Component {
+    componentDidMount() {
+        var menu = menuFile;
+        getMenuCategories(menu);
+    }
+    render() {
+        return (<div>
             <Card className="orderList_menu" sx={{ boxShadow: 3 }}>
                 <CardActions>
                     <Stack direction="row" spacing={1}>
@@ -52,9 +65,10 @@ const orderMenu = () => {
                     </Stack>
                 </CardActions>
                 <CardActions>
+                
                 </CardActions>
             </Card>
-        </div>
-    )
+        </div>)
+    }
 }
 export default orderMenu
